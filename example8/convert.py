4e72ec148ad3590d4369589d63c7459cf50aa1d2
Emily <emilycfry@ymail.com>
2021-01-15 18:27:24 +0000

    created loop, and fixed parse data

--- example8/convert.py
+++ example8/convert.py
@@ -1,40 +1,39 @@
 #!/usr/bin/env python3
  
 import csv
-import datetime
-
-# load input.csv into a variable `rows`
-with open('input.csv') as f:
-    rows = f.read()
-
-# print(rows)
-
-# parse `modeldate` in each row and sort the `rows` based on `modeldate`
 from datetime import datetime
 
-date_format = "%-d/%-m/%Y"
+# load input.csv into a variable `rows`
 
-parsed_date = datetime.strptime(row['modeldate'], date_format)
+date_format_input = "%m/%d/%Y"
+date_format_output = "%Y-%m-%d"
 
-date_str = parsed_date.strftime("%-m/%-d/%Y")
-print(date_str)
+with open('input.csv') as f:
+	reader = csv.DictReader(f)
+	rows = list(reader)
+# print(rows)
+# parse `modeldate` in each row and sort the `rows` based on `modeldate`
+	for row in rows:
+		row['modeldate'] = datetime.strptime(row['modeldate'], date_format_input)
 
-
+# sort the `rows` based on `modeldate`
+rows = sorted(rows, key=lambda x: x['modeldate'])
 
 # loop through the rows and output a csv, `output.csv` with the date and 6 different series:
 
 with open('output.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(['year','variableA','variableB','variableC','variableD', 'variableE', 'variableF'])
+	writer = csv.writer(f)
+	writer.writerow(['year','variableA','variableB','variableC','variableD', 'variableE', 'variableF'])
 
-    for row in rows:
-        writer.writerow([
+	for row in rows:
+		writer.writerow([
+			row['modeldate'].strftime(date_format_output),
 			row['approve_estimate'],
 			row['disapprove_estimate'],
 			row['approve_hi'],
 			row['approve_lo'],
 			row['disapprove_hi'],
 			row['disapprove_lo']
+		])
 
 # don't forget the csv header. check dummy.csv
-
